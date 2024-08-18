659c6cb157933a104389eec52e5209a63fd09c06
jwq <jwq@atguigu.com>
2024-08-18 22:46:02 +0800

    teat.java

---
+++ src/test.java
@@ -0,0 +1,19 @@
+
+public class test {
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      String s = new String("aaa");
+      System.out.println("s = " + s);
+
+      //TODO: 测试头都
+      if (s.isEmpty()) {
+         System.out.println("hello");
+      }else{
+         System.out.println("test.main()");
+      }
+    
+   }
+
+}
